fixing deep poly bounds backpropagation

code/analyze/deeppoly/domain.py
@@ -29,13 +29,14 @@ class RelationalConstraints(ABC):
 class GreaterThanConstraints(RelationalConstraints):
     """
         Describes "greater than" relational constraints.
-        i.e. the constraints for the N nodes at layer L are given by A_{>} * x^0 + c^L  >= x^L
+        i.e. the constraints for the N nodes at layer L are given by A_{>} * x^{L-1} + c^L  >= x^L
     """
 
     def compute_bounds(self, input_lower_bounds, input_upper_bounds):
         """ Returns the UPPER bounds for the N nodes x_i in the layer L
             i.e. l={l_i, i=1,..,N}, such that x^L_i <= l_i
         """
+
         A_pos, A_neg = TensorUtils.split_positive_negative(self.A)
         upper_bounds = torch.matmul(A_pos, input_upper_bounds) + torch.matmul(A_neg, input_lower_bounds) + self.v
         return upper_bounds
@@ -107,9 +108,9 @@ class AbstractDomain:
     """
         The abstract domain after layer L. For each node x^L_i in layer L, we have:
         - lower_bounds[i] <= x^L_i <= upper_bounds[i]
-        - x^L_i >= lower_than.A * x^0 + lower_than.v[i]
-        - x^L_i <= greater_than.A * x^0 + greater_than.v[i]
-          where x^0 are the nodes of the input layer (i.e. backsubstitution is performed at each step).
+        - x^L_i >= lower_than.A * x^{L-1} + lower_than.v[i]
+        - x^L_i <= greater_than.A * x^{L-1} + greater_than.v[i]
+          where x^{L-1} are the nodes of the previous layer
     """
     lower_bounds: torch.Tensor
     upper_bounds: torch.Tensor

code/analyze/deeppoly/transform/affine.py
@@ -7,7 +7,7 @@ from analyze.deeppoly.transform import Transformer
 from analyze.utils import TensorUtils
 
 
-class AffineTransformer(Transformer):
+class LinearTransformer(Transformer):
 
     def __init__(self, layer, backprop=False):
         self.backprop = backprop
@@ -21,35 +21,24 @@ class AffineTransformer(Transformer):
             self.bias = torch.zeros((layer.out_features, 1))
         self.N = layer.out_features
 
-    def _transform(self, ad, input, ads=None):
 
-        A_gt = torch.matmul(self.weights_pos, ad.greater_than.A) + torch.matmul(self.weights_neg, ad.lower_than.A)
-        v_gt = self.bias + torch.matmul(self.weights_pos, ad.greater_than.v) + torch.matmul(self.weights_neg, ad.lower_than.v)
-        gt = GreaterThanConstraints(A_gt, v_gt, self.weights.T, self.bias.T)
-
-        A_lt = torch.matmul(self.weights_pos, ad.lower_than.A) + torch.matmul(self.weights_neg, ad.greater_than.A)
-        v_lt = self.bias + torch.matmul(self.weights_pos, ad.lower_than.v) + torch.matmul(self.weights_neg, ad.greater_than.v)
-        lt = LowerThanConstraints(A_lt, v_lt, self.weights.T, self.bias.T)
+    def _transform(self, ads):
+        greater_than = GreaterThanConstraints(self.weights, self.bias)
+        lower_than = LowerThanConstraints(self.weights, self.bias)
 
         if self.backprop:
-            upper_bound = gt.compute_bounds_backprop(ad, ads)
-            lower_bound = lt.compute_bounds_backprop(ad, ads)
+            lower_bounds, upper_bounds = self.compute_bounds(ads, lower_than, greater_than)
         else:
-            upper_bound = gt.compute_bounds(input.lower_bounds, input.upper_bounds)
-            lower_bound = lt.compute_bounds(input.lower_bounds, input.upper_bounds)
+            ad = ads[-1]
+            upper_bounds = greater_than.compute_bounds(ad.lower_bounds, ad.upper_bounds)
+            lower_bounds = greater_than.compute_bounds(ad.lower_bounds, ad.upper_bounds)
 
-        ad_lin = AbstractDomain(lower_bound, upper_bound, lt, gt)
+        ad_lin = AbstractDomain(lower_bounds, upper_bounds, lower_than, greater_than)
         return ad_lin
 
 
-#TODO
 
-class LinearTransformer(AffineTransformer):
+
+class Conv2dTransformer(Transformer):
     pass
-
-
-class Conv2dTransformer(AffineTransformer):
-    pass
-
-
 

code/analyze/deeppoly/transform/preprocess.py
@@ -7,9 +7,8 @@ from analyze.deeppoly.transform import Transformer
 
 class Preprocessor(Transformer):
 
-    def _transform(self, ad, input, ads=None):
+    def _transform(self, ads):
         raise NotImplementedError()
-
 
 
 class NormalizeTransformer(Preprocessor):
@@ -20,8 +19,7 @@ class NormalizeTransformer(Preprocessor):
         self._l = norm_layer
 
     def transform(self, abstract_domains):
-        return AbstractDomain.preprocess(abstract_domains[-1], self._l.forward)
-
+        return [AbstractDomain.preprocess(abstract_domains[-1], self._l.forward)]
 
 
 class FlattenTransformer(Preprocessor):
@@ -30,6 +28,10 @@ class FlattenTransformer(Preprocessor):
         def flatten(a):
             f = torch.flatten(a)
             return f.reshape(f.shape[0], 1)
-        return AbstractDomain.preprocess(abstract_domains[-1], flatten)
+        ad = AbstractDomain.preprocess(abstract_domains[-1], flatten)
+        if abstract_domains[-1].has_constraints():
+            # we are not in preprocessing step
+            return abstract_domains + [ad]
+        return [ad]
 
 

code/test/toy_nn.py
@@ -71,7 +71,7 @@ class ToyNNsTestCase(unittest.TestCase):
         """
             Verifying toy NN of the RIAI course, exercise sheet 07 Problem 2
         """
-        dp = DeepPoly(save_intermediate_steps=True, relu_heuristics=Zero())
+        dp = DeepPoly(relu_heuristics=Zero())
         res, ads, steps = \
             dp.verify(ToyNNEx07(), torch.zeros((2, 1)), 1, 0, domain_bounds=[0, 1], robustness_fn=torch.greater_equal)
 
@@ -86,7 +86,7 @@ class ToyNNsTestCase(unittest.TestCase):
         """
             Verifying toy NN of the RIAI course - lecture 7 deeppoly slides 8
         """
-        dp = DeepPoly(save_intermediate_steps=True, relu_heuristics=Zero())
+        dp = DeepPoly(relu_heuristics=Zero())
         res, ads, steps = dp.verify(ToyNNCourse(), torch.zeros((2, 1)), 1, 0, domain_bounds=[-1, 1])
 
         expected_lower_bounds = [[-1, -1], [-2, -2], [0, 0], [-0.5, -2], [0, 0], [0.5, 0], [0.5]]
@@ -100,7 +100,7 @@ class ToyNNsTestCase(unittest.TestCase):
         """
             Verifying toy NN of the Deep poly paper page 41:5
         """
-        dp = DeepPoly(save_intermediate_steps=True, relu_heuristics=Zero())
+        dp = DeepPoly(relu_heuristics=Zero())
         res, ads, steps = dp.verify(ToyNNPaper(), torch.zeros((2, 1)), 1, 0, domain_bounds=[-1, 1])
 
         expected_lower_bounds = [[-1, -1], [-2, -2], [0, 0], [0, -2], [0, 0], [1.0, 0], [1]]